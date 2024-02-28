6f9d9573d3a14f3a256f4252bd7007731945ce0d
wuguoxing <1034370502@qq.com>
2024-02-28 22:58:16 +0800

    init

---
+++ src/main/java/com/danstar/blog/server/DanstarBlogServerApplication.java
@@ -0,0 +1,13 @@
+package com.danstar.blog.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanstarBlogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanstarBlogServerApplication.class, args);
+    }
+
+}
